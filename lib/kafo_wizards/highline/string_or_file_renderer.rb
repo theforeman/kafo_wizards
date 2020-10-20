6e583a3faae9fa0207b58796f46cf79396238eb2
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-10-20 18:32:10 +0200

    Use File.exist?
    File.exists? is deprecated and triggers a warning.

--- lib/kafo_wizards/highline/string_or_file_renderer.rb
+++ lib/kafo_wizards/highline/string_or_file_renderer.rb
@@ -13,7 +13,7 @@ module KafoWizards
       def render_action(entry)
         say entry.description if entry.description
         key = ask("file path or key")
-        key = File.read(key) if File.exists?(key)
+        key = File.read(key) if File.exist?(key)
         entry.update(key.chomp)
         nil
       end
